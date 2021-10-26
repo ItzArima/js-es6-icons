9760fe9b2d761c1669c75c6637aa719e8f1b9645
ItzArima <skaypaprilia@gmail.com>
2021-10-26 17:36:30 +0200

    milestone 2 completato

---
+++ Milestone_2/main.js
@@ -0,0 +1,128 @@
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	}
+];
+
+let index = 0;
+icons.forEach(creaElementi);
+
+function creaElementi(icons,index){ 
+	document.querySelector(".main-container").innerHTML +=  `
+		<div class = "icon-container">
+			<i class = "${icons.family} ${icons.prefix+icons.name} icon"></i>
+		</div>
+	`
+}
+
+var icon = document.getElementsByClassName("icon");
+for(let i=0;i<icons.length;i++){
+	if(icons[i].type == "animal"){
+		icon[i].classList.add("animal");
+	}
+}
+
+for(let i=0;i<icons.length;i++){
+	if(icons[i].type == "vegetable"){
+		icon[i].classList.add("vegetable");
+	}
+}
+
+for(let i=0;i<icons.length;i++){
+	if(icons[i].type == "user"){
+		icon[i].classList.add("user");
+	}
+}
